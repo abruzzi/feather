c0890211779797fe1220feecda59e7d8892d7264
Qiu Juntao <juntao.qiu@gmail.com>
2013-01-23 23:17:35 +0800

    add authorized machnism to feather, change the database to postgresql

--- Gemfile
+++ Gemfile
@@ -8,12 +8,5 @@ gem 'omniauth-github'
 
 gem 'datamapper'
 
-group :production do
-    gem 'pg'
-    gem 'dm-postgres-adapter'
-end
-
-group :development, :test do
-    gem 'shotgun'
-    gem 'dm-sqlite-adapter'
-end
+gem 'pg'
+gem 'dm-postgres-adapter'
--- app.rb
+++ app.rb
@@ -10,72 +10,143 @@ require './lib/user.rb'
 require './lib/notes.rb'
 
 configure do
-    DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/notes.db")
-
+    DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/feather")
     DataMapper.finalize.auto_upgrade!
 end
 
+module Feather
+    class NoteApplication < Sinatra::Base
+        use Rack::Session::Cookie
+        use OmniAuth::Builder do
+            provider :github, 'a5e58cdb3b2c6bebbdb7', '30ebed2a49aef7be26e6866caa3da619073fe951',
+                scope: "user,repo,gist"
+        end
 
-before do
-    content_type :json
-end
+        # auth callback
+        [:get, :post].each do |method|
+            send method, '/auth/:provider/callback' do
+                auth = request.env['omniauth.auth'].info
+                session[:user] = {:name => auth.name, :email => auth.email}
+                @current_user = User.all(:email => session[:user][:email])
+            end
+        end
 
-# get all notes belong to a user
-get '/users/:userid/notes' do
-    user = User.get params[:userid]
-    @notes = Note.all(:user => user, :order => :id.desc) || []
-    @notes.to_json
-end
+        # if auth failure
+        get '/auth/failure' do
+            "#{params[:message]}"
+            redirect '/'
+        end
 
-# get a signle note belongs to a user
-get '/users/:userid/notes/:noteid' do
-    user = User.get params[:userid]
-    @note = Note.all :user => user, :id => params[:noteid]
-    @note.to_json
-end
+        register do
+            def check name
+                condition do
+                    redirect '/login' unless send(name) == true
+                end
+            end
+        end
 
-# create a new note for user
-post '/users/:userid/notes' do
-    user = User.get params[:userid]
-    @note = Note.new
-    @note.complete = false
-    @note.content = params[:content]
-    @note.created_at = Time.now
-    @note.updated_at = Time.now
-    @note.user = user 
+        helpers do
+            def valid?
+                session[:user] != nil
+            end
+        end
 
-    if @note.save
-        {:note => @note, :status => 'success'}.to_json
-    else
-        {:note => @note, :status => 'failure'}.to_json
-    end
-end
+        before do
+            content_type :json
+        end
 
-# update an existing note
-put '/users/:userid/notes/:noteid' do
-    user = User.get params[:userid]
-    @note = Note.get params[:noteid]
+        not_found do
+            "404"
+        end
 
-    @note.complete = params[:complete]
-    @note.content = params[:content]
-    @note.updated_at = Time.now
-    @note.user = user 
+        error do
+            "error"
+        end
 
-    if @note.save
-        {:note => @note, :status => 'success'}.to_json
-    else
-        {:note => @note, :status => 'failure'}.to_json
-    end
-end
+        # get all notes belong to a user
+        get '/users/:userid/notes', :check => :valid? do
+            if @current_user[:id] == params[:userid]
+                user = User.get params[:userid]
+                @notes = Note.all(:user => user, :order => :id.desc) || []
+                @notes.to_json
+            end
+        end
 
-# delete an existing note
-delete '/users/:userid/notes/:noteid' do
-    user = User.get params[:userid]
-    @note = Note.get params[:noteid]
+        # get a signle note belongs to a user
+        get '/users/:userid/notes/:noteid', :check => :valid? do
+            if @current_user[:id] == params[:userid]
+                user = User.get params[:userid]
+                @note = Note.all :user => user, :id => params[:noteid]
+                @note.to_json
+            end
+        end
 
-    if @note.destroy
-        {:note => @note, :status => 'success'}.to_json
-    else
-        {:note => @note, :status => 'failure'}.to_json
+        # create a new note for user
+        post '/users/:userid/notes', :check => :valid? do
+            if @current_user[:id] == params[:userid]
+                user = User.get params[:userid]
+                @note = Note.new
+                @note.complete = false
+                @note.content = params[:content]
+                @note.created_at = Time.now
+                @note.updated_at = Time.now
+                @note.user = user 
+
+                if @note.save
+                    {:note => @note, :status => 'success'}.to_json
+                else
+                    {:note => @note, :status => 'failure'}.to_json
+                end
+            end
+        end
+
+        # update an existing note
+        put '/users/:userid/notes/:noteid', :check => :valid? do
+            if @current_user[:id] == params[:userid]
+                user = User.get params[:userid]
+                @note = Note.get params[:noteid]
+
+                @note.complete = params[:complete]
+                @note.content = params[:content]
+                @note.updated_at = Time.now
+                @note.user = user 
+
+                if @note.save
+                    {:note => @note, :status => 'success'}.to_json
+                else
+                    {:note => @note, :status => 'failure'}.to_json
+                end
+            end
+        end
+
+        # delete an existing note
+        delete '/users/:userid/notes/:noteid', :check => :valid? do
+            if @current_user[:id] == params[:userid]
+                user = User.get params[:userid]
+                @note = Note.get params[:noteid]
+
+                if @note.destroy
+                    {:note => @note, :status => 'success'}.to_json
+                else
+                    {:note => @note, :status => 'failure'}.to_json
+                end
+            end
+        end
+
+        get '/' do
+            content_type :html
+            haml :index
+        end
+
+        get '/login' do
+            content_type :html
+            haml :auth
+        end
+
+        get '/logout' do
+            session.clear
+            redirect '/'
+        end
+
     end
 end
