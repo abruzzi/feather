87a8130f4a20be20d96c4815379dee1f116e0c2e
Qiu Juntao <juntao.qiu@gmail.com>
2013-02-05 20:41:02 +0800

    fix issue when client sent PUT request

--- app.rb
+++ app.rb
@@ -20,17 +20,17 @@ module Feather
     class NoteApplication < Sinatra::Base
         use Rack::PostBodyContentTypeParser
 
-        if not test?
+        if not test?# {{{
             use Rack::Session::Cookie, :secret => 's3c23t'
             use OmniAuth::Builder do
                 provider :github, 'a5e58cdb3b2c6bebbdb7', 
                     'f8aa1d494bc731f448679620d8bd8222b3285bbf',
                     scope: "user,repo,gist"
             end
-        end
+        end# }}}
 
         # auth callback
-        [:get, :post].each do |method|
+        [:get, :post].each do |method|# {{{
             send method, '/auth/:provider/callback' do
                 auth = request.env['omniauth.auth'].info
 
@@ -45,7 +45,7 @@ module Feather
                 p session[:user]
                 redirect '/'
             end
-        end
+        end# }}}
 
         # if auth failure
         get '/auth/failure' do
@@ -61,7 +61,7 @@ module Feather
             end
         end
 
-        helpers do
+        helpers do# {{{
             def valid?
                 session[:user] != nil
             end
@@ -69,34 +69,30 @@ module Feather
             def current_user
                 User.first(:email => session[:user])
             end
-        end
+        end# }}}
 
         before do
             content_type :json
         end
 
-        not_found do
-            "404"
-        end
+        not_found { "404" }
 
-        error do
-            "error"
-        end
+        error { "error" }
 
         # get all notes belong to a user
-        get '/notes', :check => :valid? do
+        get '/notes', :check => :valid? do# {{{
             notes = Note.all(:user => current_user, :order => :id.desc) || []
             notes.to_json
-        end
+        end# }}}
 
         # get a signle note belongs to a user
-        get '/notes/:noteid', :check => :valid? do
+        get '/notes/:noteid', :check => :valid? do# {{{
             note = Note.all :user => current_user, :id => params[:noteid]
             note.to_json
-        end
+        end# }}}
 
         # create a new note for user
-        post '/notes', :check => :valid? do
+        post '/notes', :check => :valid? do# {{{
             note = Note.create(:complete => params[:complete], 
                               :content => params[:content],
                               :created_at => Time.now,
@@ -108,15 +104,16 @@ module Feather
                 p note.errors
                 {:note => note, :status => 'failure'}.to_json
             end
-        end
+        end# }}}
 
         # update an existing note
-        put '/notes/:noteid', :check => :valid? do
+        put '/notes/:noteid', :check => :valid? do# {{{
             note = Note.get params[:noteid]
 
-            p params
-            note.content = params[:content] || note.content
-            note.complete = params[:complete] || note.complete
+            data = JSON.parse(request.body.read)
+
+            note.content = data[:content] || note.content
+            note.complete = data.has_key?("complete") ? data["complete"] : note.complete
             note.updated_at = Time.now
             note.user = current_user
 
@@ -126,10 +123,10 @@ module Feather
                 p note.errors
                 {:note => note, :status => 'failure'}.to_json
             end
-        end
+        end# }}}
 
         # delete an existing note
-        delete '/notes/:noteid', :check => :valid? do
+        delete '/notes/:noteid', :check => :valid? do# {{{
             note = Note.get params[:noteid]
             if current_user == note.user
                 if note.destroy
@@ -139,7 +136,7 @@ module Feather
                     {:note => note, :status => 'failure'}.to_json
                 end
             end
-        end
+        end# }}}
 
         get '/' do
             if current_user != nil
